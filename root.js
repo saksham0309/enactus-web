4b798bddca984f5194b7fbd68355a5413f1710f6
Abhishek Satapathy <abhishek.satapathy01@gmail.com>
2019-05-06 09:41:49 +0000

    Design Changes and added routes

--- root.js
+++ root.js
@@ -4,4 +4,21 @@ const request = require('request');
 router.get("/",function(req,res){
     res.render("index");
 });
+
+router.get("/about",function(req,res){
+    res.render("index");
+});
+
+router.get("/projects",function(req,res){
+    res.render("index");
+});
+
+router.get("/events",function(req,res){
+    res.render("index");
+});
+
+router.get("/gallery",function(req,res){
+    res.render("index");
+});
+
 module.exports = router;
