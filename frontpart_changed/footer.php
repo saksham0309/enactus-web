3a57bdf95449eb62a26b99caf322f6247b484f7e
Soumitro Datta <50131027+soumi0705@users.noreply.github.com>
2019-05-14 23:01:03 +0530

    Header And Footer Just Changed To PHP

---
+++ frontpart_changed/footer.php
@@ -0,0 +1,110 @@
+<!--================ Start Subscribe Area =================-->
+<div class="container" name = "contactus" id= "contactus">
+    <div class="subscribe_area">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="d-flex align-items-center">
+                    <h1 class="text-white">Do you have a question?</h1>
+                    <div id="mc_embed_signup">
+                        <form target="_blank" action="#" method="get" class="subscribe_form relative">
+                            <div class="input-group d-flex flex-row">
+                                <br>
+                                <input name="EMAIL" placeholder="Your email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+
+                            <button class="ml-10 primary_btn yellow_btn btn sub-btn rounded">SUBSCRIBE</button>
+                            </div>
+                            <div class="info"></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--================ End Subscribe Area =================-->
+
+<!--================ Start footer Area  =================-->
+<footer>
+    <div class="footer-area">
+        <div class="container">
+            <div class="row section_gap">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-footer-widget tp_widgets">
+                        <h4 class="footer_title large_title">Our Mission</h4>
+                        <p>
+                            We are an international non-profit organisation that believes investing in students who take ENTrepreneurial ACTion for uplifting a part of a society, hence, creating a better world for US all.
+                        </p>
+
+                    </div>
+                </div>
+                <div class="offset-lg-1 col-lg-2 col-md-6 col-sm-6">
+                    <div class="single-footer-widget tp_widgets">
+                        <h4 class="footer_title">Quick Links</h4>
+                        <ul class="list">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Projects</a></li>
+                            <li><a href="#">Events</a></li>
+                            <li><a href="#">Gallery</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6 col-sm-6">
+                    <div class="single-footer-widget instafeed">
+                        <h4 class="footer_title">Gallery</h6>
+                        <ul class="list instafeed d-flex flex-wrap">
+                            <li><img src="/img/gallery/g1.jpg" alt=""></li>
+                            <li><img src="/img/gallery/g2.jpg" alt=""></li>
+                            <li><img src="/img/gallery/g3.jpg" alt=""></li>
+                            <li><img src="/img/gallery/g4.jpg" alt=""></li>
+                            <li><img src="/img/gallery/g5.jpg" alt=""></li>
+                            <li><img src="/img/gallery/g6.jpg" alt=""></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="offset-lg-1 col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-footer-widget tp_widgets">
+                        <h4 class="footer_title">Contact Us</h4>
+                        <div class="ml-40">
+                            <p class="sm-head">
+                                <span class="fa fa-location-arrow"></span>
+                                Head Office
+                            </p>
+                            <p>VIT CHENNAI, VANDALUR KELAMBAKKAM ROAD ,CHENNAI 600048</p>
+
+                            <p class="sm-head">
+                                <span class="fa fa-phone"></span>
+                                Phone Number
+                            </p>
+                            <p>
+                                +123 456 7890 <br>
+                                +123 456 7890
+                            </p>
+
+                            <p class="sm-head">
+                                <span class="fa fa-envelope"></span>
+                                Email
+                            </p>
+                            <p>
+                                  <a href="mailto:enactus.vitc@gmail.com">  enactus.vitc@gmail.com</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row d-flex">
+                <p class="col-lg-12 footer-text text-center">
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--================ End footer Area  =================-->
