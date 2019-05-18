eba318a782de010af7d383dec466a8a0138e4caa
abhishek <abhishek.satapathy01@gmail.com>
2019-05-19 00:14:27 +0530

    Added members page

--- aboutus.js
+++ aboutus.js
@@ -1,7 +1,43 @@
 var router = require("express").Router();
 const request = require('request');
 
-router.get("/about",function(req,res){
-    res.render("index");
+var advMembers = [{
+    name: "John Doe",
+    desg: "President",
+    desc: "bla  bka bla",
+    photo: "johndoe"
+},
+{
+    name: "Jane Doe",
+    desg: "Vice President",
+    desc: "bla  bka bla",
+    photo: "janedoe"
+}];
+
+var finMembers = [{
+    name: "Anshul",
+    desg: "President",
+    desc: "bla  bka bla",
+    photo: "johndoe"
+},
+{
+    name: "Anshul",
+    desg: "Vice President",
+    desc: "bla  bka bla",
+    photo: "janedoe"
+}];
+
+
+router.get("/about", function (req, res) {
+    res.render("aboutus",{advMembers:advMembers,finMembers:finMembers});
 });
+
+router.get("/about/adv",function(req,res){
+    res.end(JSON.stringify(advMembers));
+});
+
+router.get("/about/fin",function(req,res){
+    res.end(JSON.stringify(finMembers));
+});
+
 module.exports = router;
